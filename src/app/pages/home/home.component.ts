afd08901e430fb0965c9a08d61c4f94dbad9b04f
wil1708 <william.wpires@gmail.com>
2024-08-05 01:38:06 +0200

    90% home finished

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, TemplateRef } from '@angular/core';
 import { Color, LegendPosition, ScaleType } from '@swimlane/ngx-charts';
 import { map, Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
@@ -11,32 +11,33 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$!: Observable<Olympic[]>;
+  olympics$!: Observable<Olympic[]>;
 
   view: [number, number] = [700, 400];
   colorScheme: Color = {
     name: 'custom',
     selectable: true,
     group: ScaleType.Ordinal,
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    domain: ['#956065', '#B8CBE7', '#89A1DB', '#793d52', '#9780A1']
   };
   gradient: boolean = true;
-  showLegend: boolean = true;
-  legendPosition: LegendPosition = LegendPosition.Below;
+  showLegend: boolean = false;
   showLabels: boolean = true;
-  isDoughnut: boolean = false;
+  isDoughnut: boolean = false;  
 
-  public chartData: { name: string, value: number }[] = [];
+  //Objet itérable utilisé dans la pie charts
+  chartData: { name: string, value: number }[] = [];
 
-  constructor(private olympicService: OlympicService) {
-    Object.assign(this, { olympics$: this.olympics$ });
-  }
+  constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
 
+    this.updateViewSize();
+
+    //code initialisant l'observable olymics$
     this.olympics$ = this.olympicService.getOlympics();
-    console.log(this.olympics$);
     
+    //code initialisant chartData
     this.olympics$.pipe(
       map(olympics => olympics.map(olympic => ({
         name: olympic.country,
@@ -60,8 +61,21 @@ export class HomeComponent implements OnInit {
     console.log('Deactivate', JSON.parse(JSON.stringify(olympics$)));
   }
 
-  trackById(index: number, item: Olympic): number {
-    return item.id;
+  // trackById(index: number, item: Olympic): number {
+  //   return item.id;
+  // }
+
+  updateViewSize(): void {
+    const width = window.innerWidth * 0.9; // Adjust the multiplier as needed
+    const height = window.innerHeight * 0.5; // Adjust the multiplier as needed
+    this.view = [width, height];
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.updateViewSize();
+  }
+
+
 
 }
